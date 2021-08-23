e8ea4179391118eb4199e371f6baa620f6fc7686
unknown <sandeepshinde630@gmail.com>
2021-08-23 15:27:46 +0530

    feat:Welcome Message

---
+++ UserRegistration_MSTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration_MSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration MSTest");
+        }
+    }
+}
